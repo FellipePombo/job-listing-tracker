23f33bbd7cb2317a80f902014f67292259603e1a
FellipePombo <fellipecarvalhop@gmail.com>
2026-03-28 19:08:54 -0300

    basic surface linkedin scraping

--- scrapers/linkedin.js
+++ scrapers/linkedin.js
@@ -8,10 +8,37 @@ module.exports = async (page, params) => {
   
     await handleOptionalDialog(page);
     
-    const title = await page.title();
+    const list = await page.waitForSelector('[class="jobs-search__results-list"]', { timeout: 0 });
+    const jobs = await list.$$('.base-search-card');
+    let jobsScraped = [];
+    if(jobs.length > 0){
+      let i = 0;
+      while(i < jobs.length && i < 20){
+        let jobScraped = {};
+        const info = await jobs[i].$('.base-search-card__info');
+        console.log(info);
+        const metadata = await info.$('.base-search-card__metadata');
+
+        const title = await info.$('.base-search-card__title');
+        jobScraped["title"] = title ? await title.evaluate(el => el.innerText) : "";
+
+        const subtitle = await info.$('.base-search-card__subtitle');
+        jobScraped["subtitle"] = subtitle ? await subtitle.evaluate(el => el.innerText) : "";
+
+        const location = await metadata.$('.job-search-card__location');
+        jobScraped["location"] = location ? await location.evaluate(el => el.innerText) : "";
+
+        const benefits= await metadata.$('.job-posting-benefits__text');
+        jobScraped["benefits"]  = benefits ? await benefits.evaluate(el => el.innerText) : "";
+        jobsScraped.push(jobScraped);
+        i++;
+      }
+    }
+    console.log(jobsScraped);
+
   
     return {
-      title,
+      jobsScraped,
       timestamp: new Date()
     };
   };
@@ -60,7 +87,7 @@ module.exports = async (page, params) => {
     
     for (const key in LINKEDIN_MAPPINGS){
         if (key == "datePosted"){
-            link += `f_SB2=${LINKEDIN_MAPPINGS["datePosted"][params["datePosted"]]}&`
+            link += `f_TPR=${LINKEDIN_MAPPINGS["datePosted"][params["datePosted"]]}&`
         }else{
             link += buildParam(key,params);
         }
